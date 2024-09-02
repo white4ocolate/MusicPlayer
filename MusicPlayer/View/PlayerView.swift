9655fd3a49673845c270ad5874be7d5a004782bb
white4ocolate <morozovyuriy11@gmail.com>
2024-09-02 13:12:55 +0300

    view list tracks

--- MusicPlayer/View/PlayerView.swift
+++ MusicPlayer/View/PlayerView.swift
@@ -11,6 +11,23 @@ struct PlayerView: View {
     var body: some View {
         ZStack {
             BackgroundView()
+            List {
+                HStack {
+                    Color(.orange)
+                        .frame(width: 60, height: 60)
+                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    VStack(alignment: .leading) {
+                        Text("Track Name")
+                            .font(.title3)
+                        Text("Artist")
+                            .font(.subheadline)
+                    }
+                    Spacer()
+                    Text("03:40")
+                }
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
+            }.listStyle(.plain)
         }
     }
 }
